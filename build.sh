77774eb1efed5ed17857db5a3eba4a78a49dcebe
Oliver Kopp <kopp.dev@gmail.com>
2023-07-09 17:56:02 +0200

    Fix biber execution

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
+set -e
 pdflatex lni.dtx
 pdflatex lni.dtx
 pdflatex lni.dtx
 touch mybibfile.bib
 pdflatex lni-author-template
 pdflatex lni-paper-example-de
-pdflatex lni-paper-example-de
 biber lni-paper-example-de
+pdflatex lni-paper-example-de
 pdflatex lni-paper-example-de
