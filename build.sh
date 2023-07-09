311832d912a5b3af323041f3b2b44c87a1ed403e
Oliver Kopp <kopp.dev@gmail.com>
2023-07-09 16:42:04 +0200

    Try to finish workflow

---
+++ build.sh
@@ -0,0 +1,9 @@
+pdflatex lni.dtx
+pdflatex lni.dtx
+pdflatex lni.dtx
+touch mybibfile.bib
+pdflatex lni-author-template
+pdflatex lni-paper-example-de
+biber lni-paper-example-de
+pdflatex lni-paper-example-de
+pdflatex lni-paper-example-de
