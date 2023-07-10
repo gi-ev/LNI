28f75d1151de505e971b80a0bb0dbae07f49150a
Oliver Kopp <kopp.dev@gmail.com>
2023-07-10 09:55:36 +0200

    Add guard "\ifusebiblatex"

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ pdflatex lni.dtx
 pdflatex lni.dtx
 pdflatex lni.dtx
 touch mybibfile.bib
-printf "\nCompiling lni-author-template...\n"
+printf "\nCompiling lni-author-template...\n\n"
 pdflatex lni-author-template
-printf "\nCompiling lni-paper-example-de...\n"
+printf "\nCompiling lni-paper-example-de...\n\n"
 pdflatex lni-paper-example-de
-printf "\nBiber lni-paper-example-de...\n"
+printf "\nBiber lni-paper-example-de...\n\n"
 biber lni-paper-example-de
-printf "\nCompiling lni-paper-example-de...\n"
+printf "\nCompiling lni-paper-example-de...\n\n"
 pdflatex lni-paper-example-de
-printf "\nCompiling lni-paper-example-de...\n"
+printf "\nCompiling lni-paper-example-de...\n\n"
 pdflatex lni-paper-example-de
