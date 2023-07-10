d2e99eecb121a67f8ace48eafbf7775ff727203a
Oliver Kopp <kopp.dev@gmail.com>
2023-07-10 09:43:32 +0200

    Some more structured output in build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@ pdflatex lni.dtx
 pdflatex lni.dtx
 pdflatex lni.dtx
 touch mybibfile.bib
+printf "\nCompiling lni-author-template...\n"
 pdflatex lni-author-template
+printf "\nCompiling lni-paper-example-de...\n"
 pdflatex lni-paper-example-de
+printf "\nBiber lni-paper-example-de...\n"
 biber lni-paper-example-de
+printf "\nCompiling lni-paper-example-de...\n"
 pdflatex lni-paper-example-de
+printf "\nCompiling lni-paper-example-de...\n"
 pdflatex lni-paper-example-de
