d3e81160309a0778d6230fe92b06eb70f4288dd9
Oliver Kopp <kopp.dev@gmail.com>
2023-07-12 11:28:57 +0200

    Can't check with git

--- build.sh
+++ build.sh
@@ -7,11 +7,6 @@ pdflatex lni.dtx
 pdflatex lni.dtx
 pdflatex lni.dtx
 
-if ! git diff-index --quiet HEAD --; then
-  echo -e "\033[31mRepository is not clean, please adapt lni.dtx, too.\033[37m"
-  exit 1
-fi
-
 printf "\n\033[32mCompiling lni-author-template...\n\n\033[37m"
 touch mybibfile.bib
 pdflatex lni-author-template
